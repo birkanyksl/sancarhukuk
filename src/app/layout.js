cda0d827f3413dd9b45e94d1a33ba34328ea496e
Birkan <birkanyksl613@gmail.com>
2024-09-05 17:25:14 +0300

    Navbar Update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="w-full bg-white px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 ">
+        <Navbar/>
+          </div> 
+        {children}</body>
     </html>
   );
 }
---
+++ src/components/MobileMenu.jsx
@@ -0,0 +1,32 @@
+"use client"
+import Link from 'next/link'
+import React, { useState } from 'react'
+
+const MobileMenu = () => {
+const [isOpen,setIsOpen] = useState(false)
+
+  return (
+    <div className=''>
+        <div className='flex flex-col gap-[4.5px] cursor-pointer' onClick={()=>setIsOpen(prevstate =>!prevstate)}>
+            <div className={`w-6 h-1 bg-blue-500 rounded-sm ${isOpen ? "rotate-45": ""} origin-left ease-in-out duration-500` }/>
+
+            <div className={`w-6 h-1 bg-blue-500 rounded-sm ${isOpen ? "opacity-0": ""} ease-in-out duration-500`}/>
+
+            <div className={`w-6 h-1 bg-blue-500 rounded-sm ${isOpen ? "-rotate-45": ""} origin-left ease-in-out duration-500`}/>
+
+            {isOpen && (
+            <div className='absolute left-0 top-24 w-full h-[calc(100vh-96px)] bg-white flex flex-col items-center justify-center gap-8 font-medium text-xl z-10'>
+                <Link href="/">Hakkımızda</Link>
+                <Link href="/">Ekibimiz</Link>
+                <Link href="/">Hizmetler</Link>
+                <Link href="/">Yayınlar</Link>
+                <Link href="/">İletişim</Link>
+
+            </div>
+            )}
+        </div>
+    </div>
+  )
+}
+
+export default MobileMenu
