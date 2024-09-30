81f82123508f56ed482a6bdd2e87e3958ded044b
Birkan <birkanyksl613@gmail.com>
2024-09-30 16:55:32 +0300

    Update AboutPageTeam.jsx

--- src/components/about/AboutPageTeam.jsx
+++ src/components/about/AboutPageTeam.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const AboutPageTeam = () => {
   return (
-    <div className='px-6 pb-8 md:px-8 lg:px-16 flex flex-row gap-2 my-8 items-center'>
+    <div className='px-6 pb-8 md:px-8 lg:px-16 flex flex-row gap-2 my-8 items-center md:my-20'>
       
-      <div className='flex flex-col w-1/2 lg:w-1/3 h-96 p-4  [border-radius:0px_200px_200px_0px] bg-amber-300 text-center justify-center items-center cursor-pointer shadow-lg '>
+      <div className='flex flex-col w-1/2 lg:w-1/3 h-72 p-4  [border-radius:0px_200px_200px_0px] bg-amber-300 text-center justify-center items-center cursor-pointer shadow-lg '>
         <h2 className='text-2xl lg:text-3xl xl:text-4xl font-semibold mr-4 text-color7'>Meet Our Team</h2>
        
       </div>
@@ -32,7 +32,7 @@ const AboutPageTeam = () => {
           />
         </div>
       </div>
-      <div className='hidden w-1/3 h-96 pl-12 border lg:flex [border-radius:200px_0px_0px_200px] bg-amber-300 justify-center items-center cursor-pointer shadow-lg'>
+      <div className='hidden w-1/3 h-72 pl-12 border lg:flex [border-radius:200px_0px_0px_200px] bg-amber-300 justify-center items-center cursor-pointer shadow-lg'>
       <p className='text-lg font-semibold  text-color7'>
       Our diverse team is committed to delivering outstanding results and innovative solutions.</p>
        
