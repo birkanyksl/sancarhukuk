dfaa1aa679135be5ddfd6d98713db166abb52a3f
Birkan <birkanyksl613@gmail.com>
2024-10-04 05:28:00 +0300

    api

--- src/components/insight/posts/Posts.jsx
+++ src/components/insight/posts/Posts.jsx
@@ -1,10 +1,13 @@
+
 import Link from 'next/link';
 import Post from '../post/Post';
 import { useLocale } from 'next-intl';
 import { posts } from '@/data/posts';
 
+
 const Posts = () => {
     const locale = useLocale()
+
   return (
     <div className='grid md:grid-cols-2 lg:grid-cols-3 items-center px-6 md:px-8 lg:px-16'>
       {posts.map(post => (
