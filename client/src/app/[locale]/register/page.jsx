a67a781e4d88192f939b31d0ff22b73856ebd369
Birkan <birkanyksl613@gmail.com>
2024-11-15 06:51:11 +0300

    Update page.jsx

--- client/src/app/[locale]/register/page.jsx
+++ client/src/app/[locale]/register/page.jsx
@@ -1,16 +1,9 @@
-import { redirect } from "next/navigation";
-import { user } from "@/utils/auth";
-import { useLocale } from "next-intl";
 import Register from "@/components/register/Register";
 
 
-const RegisterPage = () => {
-  const locale = useLocale();
-  const user1 = user;
 
-  if (user1) {
-    redirect(`/${locale}/`);
-  }
+const RegisterPage = () => {
+
 
   return (
     <div>
