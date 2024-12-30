6231a9d27e9eebd888b5668c2a4478e7d88262d9
Birkan <birkanyksl613@gmail.com>
2024-12-30 11:43:52 +0300

    update locale

--- client/src/app/[locale]/practice/page.jsx
+++ client/src/app/[locale]/practice/page.jsx
@@ -1,6 +1,7 @@
 import Breadcrumb from "@/components/BreadCrumb";
 import Practice from "@/components/practice/Practice";
 import React from "react";
+import { useTranslations } from "next-intl";
 
 export const metadata = {
   title: "Hizmetler - Yüksel Hukuk Bürosu | İstanbul",
@@ -15,17 +16,17 @@ export const metadata = {
   },
 };
 const PracticePage = () => {
+  const t = useTranslations("practicePage")
   return (
     <>
       <div className="relative w-full h-48 bg-slate-50">
         <div className="absolute inset-0 flex items-center justify-center">
           <div className="text-color1 p-4  text-center">
           <h1 className="text-4xl lg:text-5xl font-medium">
-              <span className="text-color6">Hizmetler</span>
+              <span className="text-color6">{t("headerText")}</span>
             </h1>
             <p className="mt-4 text-sm lg:text-base text-gray-600">
-              Şirketimiz, kapsamlı hukuki danışmanlık ve dava hizmetleri sunarak
-              müvekkillerine güvenilir çözümler sağlar.
+            {t("headerSubText")}
             </p>
           </div>
         </div>
